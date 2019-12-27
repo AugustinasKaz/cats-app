c034d3431544bacc114d1fd85af2331079ea8bab
Kaziux <pelenyt3@gmail.com>
2019-12-27 10:57:41 +0200

    request

--- index.js
+++ index.js
@@ -1,35 +1,17 @@
 const express = require('express');
 const request = require('request');
 const app = express();
-const port = 5000;
-const { Client } = require('pg');
+const port = process.env.PORT || 5000;
 
-const DATABASE_URL = 'postgres://xawgefxcwnxfqh:c5abc23f115270d5f68d9664042a0b9c0d7e7646f4f44018b3a0e3f4e5aaebe2@ec2-176-34-184-174.eu-west-1.compute.amazonaws.com:5432/d5u7va0o6g0486';
+app.use(express.static(path.join(__dirname, 'client/build')));
+
+
 var options = {
     method: 'GET',
     url: 'https://api.thecatapi.com/v1/images/search?limit=21',
     headers: { 'x-api-key': '6b20cc7c-275a-4071-b1dc-e12315b7da18' }
 };
 
-app.get('/api/users', (req, res) => {
-    const client = new Client({
-        connectionString: DATABASE_URL,
-        ssl: true,
-    });
-    client.connect();
-    client.query('SELECT*FROM users;', (err, response) => {
-        if (err)
-            throw err;
-        else {
-            const users = [];
-            for (let row of response.rows) {
-                users.push(row);
-            }
-            client.end();
-            res.json(users);
-        }
-    });
-})
 
 app.get('/api/cats', (req, res) => {
     request(options, function (error, response, body) {
@@ -47,7 +29,10 @@ app.get('/api/cats', (req, res) => {
         }
 
     });
-
 })
+
+app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname + '/client/build/index.html'));
+});
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`))
