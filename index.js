2fe81662004ab9a7482bccaed3a0b44d7b5a260d
Kaziux <pelenyt3@gmail.com>
2020-02-08 11:45:49 +0200

    heroku deply update

--- index.js
+++ index.js
@@ -1,11 +1,11 @@
 const express = require('express');
 const request = require('request');
 const path = require('path');
-const app = express();
-var cors = require('cors');
+//var cors = require('cors');
 var bodyParser = require("body-parser");
 const port = process.env.PORT || 5000;
 const { Client } = require('pg');
+const app = express();
 
 const DATABASE_URL = 'postgres://yamrchswqjoioq:208b0683ad99399eb224663ed3235d1152cfeb501cad220c54933fbb889ef381@ec2-184-72-236-57.compute-1.amazonaws.com:5432/db6cllld6q1vh0';
 var options = {
@@ -14,7 +14,7 @@ var options = {
     headers: { 'x-api-key': '6b20cc7c-275a-4071-b1dc-e12315b7da18' }
 };
 
-app.use(cors());
+//app.use(cors());
 app.use(express.static(path.join(__dirname, 'my-app/build')));
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
--- my-app/src/components/APIfunctions.js
+++ my-app/src/components/APIfunctions.js
@@ -1,7 +1,7 @@
 import axios from 'axios'
 
 export async function PostComment(name, comment) {
-    const promise = await axios.post("http://127.0.0.1:5000/api/post_comment", { user: name, comment: comment });
+    const promise = await axios.post("/api/post_comment", { user: name, comment: comment });
     const status = promise.status
     if(status === 200){
         let res = promise.data;
@@ -13,7 +13,7 @@ export async function PostComment(name, comment) {
 }
 
 export async function GetComments() {
-    const promise = await axios.get("http://127.0.0.1:5000/api/getComments");
+    const promise = await axios.get("/api/getComments");
     const status = promise.status
     if(status === 200){
         let res = promise.data;
